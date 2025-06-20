1d5e536dadc751c8d57e37162a9559f9681cfd92
Raman Shah <ramanshah440@gmail.com>
2025-06-20 12:52:15 +0545

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,25 @@ import App from './App.jsx';
 import Physics from './pages/Physics/Physics.jsx';
 import Chemistry from './pages/Chemistry/Chemistry.jsx';
 import Math from './pages/Math/Math.jsx';
-import Computer from'./pages/computer/Computer.jsx'
+import Computer from'./pages/Computer/Computer.jsx'
 import Nepali from './pages/Nepali/Nepali.jsx'
 import English from './pages/English/English.jsx'
 function Layout() {
   return (
     <div>
-      <Routes>
-      
-<Route path="*" element={<h1>404 - Page Not Found</h1>} />
+<Routes>
+  <Route path="/" element={<App />} />
+  <Route path="/physics" element={<Physics />} />
+  <Route path="/chemistry" element={<Chemistry />} />
+  <Route path="/math" element={<Math />} />
+  <Route path="/computer" element={<Computer />} />
+  <Route path="/nepali" element={<Nepali />} />
+  <Route path="/english" element={<English />} />
+  
+  {/* 404 should be last */}
+  <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+</Routes>
 
-        <Route path="/" element={<App />} />
-        <Route path="/physics" element={<Physics />} />
-        <Route path="/chemistry" element={<Chemistry />} />
-               <Route path="/math" element={<Math/>} />
-                        <Route path="/computer" element={<Computer/>} />
-                         <Route path="/nepali" element={<Nepali/>} />
-                                     <Route path="/english" element={<English/>} />
-      </Routes>
     </div>
   );
 }
